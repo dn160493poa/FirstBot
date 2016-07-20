06aefd60bb2534553ae49b57e5315e2eb18f276e
OlegLS <dp1604poa@gmail.com>
2016-07-20 21:16:17 +0300

    m

--- main.py
+++ main.py
@@ -12,9 +12,8 @@ def test():
 @app.route("/hook", methods=["POST"])
 def hook():
     chat_id = request.get_json()["message"]["chat"]["id"]
-    requests.post("https://api.telegram.org/bot257528811:257528811:AAHbKoS1oPEcq_IQkbQ8YB-VQyoRkYTX9rQ/sendMessage",
-                {
-                    "chat_id": chat_id,
-                    "text": "hi!"
-                })
+    requests.post("https://api.telegram.org/bot257528811:AAHbKoS1oPEcq_IQkbQ8YB-VQyoRkYTX9rQ/sendMessage", {
+            "chat_id": chat_id,
+            "text": "hi!"
+        })
     return "OK"
