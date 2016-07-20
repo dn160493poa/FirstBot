b09f4dac7dc349f76ca94ca8cfaf09e6bc90406d
OlegLS <dp1604poa@gmail.com>
2016-07-20 20:33:29 +0300

    смена файла main

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from  flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def test():
     return "It work"
 
+
 @app.route("/hook", methods=["POST"])
 def hook():
     return "OK"
-
-app.run()
