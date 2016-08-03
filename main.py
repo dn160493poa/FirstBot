437791be3614f79f32057cfb06f27fe9aad26b6f
OlegLS <dp1604poa@gmail.com>
2016-08-03 19:24:44 +0300

    откат

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def test():
 @app.route("/hook", methods=["POST"])
 def hook():
     chat_id = request.get_json()["message"]["chat"]["id"]
-    requests.post("https://api.telegram.org/bot257528811:257528811:AAHbKoS1oPEcq_IQkbQ8YB-VQyoRkYTX9rQ/sendMassage",
+    requests.post("https://api.telegram.org/bot257528811:AAE1olpVb7hpblrHVr_fgRhAaloOtJ8oT4I/sendMassage",
                 {
                     "chat_id": chat_id,
                     "text": "hi!"
