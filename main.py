9f0e356c679b740861152363fba174b715035779
OlegLS <dp1604poa@gmail.com>
2016-09-18 13:42:41 +0300

    downdate

--- main.py
+++ main.py
@@ -1,99 +1,71 @@
-import requests
 from flask import Flask, request
+import requests
+from pymongo import MongoClient
 import telegram
 
-KEY = "257528811:AAFBIgIu2o1TsghEHIsLyRt0YjvZXLd2OBI"
-
 app = Flask(__name__)
-
-from pymongo import MongoClient
-client = MongoClient("mongodb://user:user@ds029665.mlab.com:29665/heroku_z79v7760")
-db = client.heroku_z79v7760
+KEY = "257528811:AAE1olpVb7hpblrHVr_fgRhAaloOtJ8oT4I"
 
 
-bot = telegram.Bot(KEY)
-
-
-@app.route("/cards", methods=["POST"])
-def add_card():
-    db.cards.insert(request.get_json())
-    return "OK"
-
-
-@app.route("/cards", methods=["GET"])
-def get_cards():
-    return str(list(db.cards.find({})))
+client = MongoClient("mongodb://dp160493poa:366619oleg@ds029675.mlab.com:29675/heroku_2hz4q0l0")
+db = client.heroku_2hz4q0l0
 
 
 @app.route("/")
 def test():
-    return "It works"
+    return "It work"
 
 
 def send(chat_id, text):
-    requests.post("https://api.telegram.org/bot{key}/sendMessage".format(key=KEY),
+    requests.post("https://api.telegram.org/bot257528811:AAE1olpVb7hpblrHVr_fgRhAaloOtJ8oT4I/sendMessage",
                   {
                       "chat_id": chat_id,
                       "text": text
                   })
 
 
-def Marker(label, lat, lng, color="red"):
-        return "&markers=size:mid|label:{label}|{lat},{lng}|color={color}".format(
-            label=label,
-            lat=lat,
-            lng=lng,
-            color=color
-        )
+def marker(label, lat, lng, color="red"):
+    return "&markers=size:mid|label:{label}|{lat},{lng}|color={color}".format(
+        label=label,
+        lat=lat,
+        lng=lng,
+        color=color
+    )
 
 
-def map(lat=0.0, lng=0.0, zoom=13, size=100, markers=[]):
-    url = "https://maps.googleapis.com/maps/api/staticmap?center={lat},{lng}&zoom={zoom}&size={size}x{size}&key=AIzaSyBEbX-HI26eD3euneOEXXphg4xT950UjC0".format(lat=lat, lng=lng, size=size, zoom=zoom)
-
-    for marker in markers:
-        url += marker
-
-    return url
-
-
-def send_photo(chat_id, url):
-    r = requests.post("https://api.telegram.org/bot{key}/sendPhoto".format(key=KEY), json={
-                      "chat_id": chat_id,
-                      "photo": url
-                  })
-    print(r)
+def sendFoto(chat_id, photo):
+    pass
 
 
 @app.route("/hook", methods=["POST"])
 def hook():
     chat_id = request.get_json()["message"]["chat"]["id"]
-    text = request.get_json()["message"].get("text")
-    location = request.get_json()["message"].get("location")
+    text = request.get_json()["message"]["text"]
 
-    if text:
-        command, *args = text.split()
+    command, *args = text.split()
 
-        if command == "/add":
-            db.products.insert({"products": args})
-            send(chat_id, "Products added.")
+    send(chat_id, "Напишите команду")
+    send(chat_id, "Командой пока что может быть название населенного пункта")
 
-        if command == "/get":
-            answer = "\n".join(
-                map(str, db.products.find())
-            )
-            send(chat_id, answer)
+    for safe in db.safes.find(
+            {"$or": [
+                {"город": command},
+                {"страна": command},
+                {"область": command},
+                {"улица": command},
+                {"район": command}
+            ]}
+    ):
+        send(chat_id, "(" + safe["тип"] + " " + "клиентов" + ")" + " " + safe["улица"] + " " + "бранч:" + " " + safe["бранч"])
 
-        if command == "map":
-            url = map(50.4116, 30.5284588, 14, 300, markers=[
-                Marker("C", 50.4116, 30.5284588),
-                Marker("A", 50.4117, 30.5284688, "green"),
-            ])
-            bot.sendPhoto(chat_id, url)
+    for safe in db.safes.find(
+            {"$or": [
+                {"бранч": command}
+            ]}
+    ):
+        pass
 
-    if location:
-        send(chat_id, "{}, {}".format(location["longitude"], location["latitude"]))
+    if command == "/Киев":
+        send(chat_id, "https://www.google.ru/maps/place/бульвар+Дружби+Народів,+4,+%D0%9A%D0%B8%D1%97%D0%B2,+%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0/@50.4116455,30.5284588,17z/data=!4m13!1m7!3m6!1s0x40d4cf3f53763da9:0xf6eca58db9696a8a!2z0LHRg9C70YzQstCw0YAg0JTRgNGD0LbQsdC4INCd0LDRgNC-0LTRltCyLCA0LCDQmtC40ZfQsiwg0KPQutGA0LDQuNC90LA!3b1!8m2!3d50.4116455!4d30.5306475!3m4!1s0x40d4cf3f53763da9:0xf6eca58db9696a8a!8m2!3d50.4116455!4d30.5306475")
+
     return "OK"
-
-
-
-
