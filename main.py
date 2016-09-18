51c50230a2acafbbc53a9ba4ccd7283f23b45e5f
OlegLS <dp1604poa@gmail.com>
2016-09-18 13:20:18 +0300

    new_test_8

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import requests
 import telegram
-import telebot
+
 
 KEY = "257528811:AAFBIgIu2o1TsghEHIsLyRt0YjvZXLd2OBI"
 
@@ -11,8 +11,8 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://dp160493poa:366619oleg@ds029675.mlab.com:29675/heroku_2hz4q0l0")
 db = client.heroku_2hz4q0l0
 
-bot = telebot.TeleBot(KEY)
 
+bot = telegram.Bot(KEY)
 
 
 @app.route("/")
