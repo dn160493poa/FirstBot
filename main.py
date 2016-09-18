4e56aef13c7a406cd439be1e55ba2a850579f1a5
OlegLS <dp1604poa@gmail.com>
2016-09-18 12:29:59 +0300

    update_1.0.9.7

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ KEY = "257528811:AAE1olpVb7hpblrHVr_fgRhAaloOtJ8oT4I"
 client = MongoClient("mongodb://dp160493poa:366619oleg@ds029675.mlab.com:29675/heroku_2hz4q0l0")
 db = client.heroku_2hz4q0l0
 
-bot = telegram.Bot(KEY)
-
 
 @app.route("/")
 def test():
@@ -58,35 +56,34 @@ def hook():
     text = request.get_json()["message"].get("text")
     location = request.get_json()["message"].get("location")
 
-
     command, *args = text.split()
 
     send(chat_id, "Напишите команду")
     send(chat_id, "Командой пока что может быть название населенного пункта")
 
-    if text:
-        command, *args = text.split()
+    for safe in db.safes.find(
+            {"$or": [
+                {"город": command},
+                {"страна": command},
+                {"область": command},
+                {"улица": command},
+                {"район": command}
+            ]}
+    ):
+        send(chat_id, "(" + safe["тип"] + " " + "клиентов" + ")" + " " + safe["улица"] + " " + "бранч:" + " " + safe["бранч"])
 
-        if command == "/add":
-            db.products.insert({"products": args})
-            send(chat_id, "Products added.")
+    for safe in db.safes.find(
+            {"$or": [
+                {"бранч": command}
+            ]}
+    ):
+        pass
 
-        if command == "/get":
-            answer = "\n".join(
-                map(str, db.products.find())
-            )
-            send(chat_id, answer)
+    if command == "/Киев":
+        send(chat_id, "https://www.google.ru/maps/place/бульвар+Дружби+Народів,+4,+%D0%9A%D0%B8%D1%97%D0%B2,+%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B0/@50.4116455,30.5284588,17z/data=!4m13!1m7!3m6!1s0x40d4cf3f53763da9:0xf6eca58db9696a8a!2z0LHRg9C70YzQstCw0YAg0JTRgNGD0LbQsdC4INCd0LDRgNC-0LTRltCyLCA0LCDQmtC40ZfQsiwg0KPQutGA0LDQuNC90LA!3b1!8m2!3d50.4116455!4d30.5306475!3m4!1s0x40d4cf3f53763da9:0xf6eca58db9696a8a!8m2!3d50.4116455!4d30.5306475")
 
-        if command == "map":
-            url = map(50.4116, 30.5284588, 14, 300, markers=[
-                Marker("C", 50.4116, 30.5284588),
-                Marker("A", 50.4117, 30.5284688, "green"),
-            ])
-            bot.sendPhoto(chat_id, url)
-
-    if location:
-        send(chat_id, "{}, {}".format(location["longitude"], location["latitude"]))
     return "OK"
+
 
 
 
